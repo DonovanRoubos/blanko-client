1cd2a21b295edc1410116990fb829f61a1a19947
Donovan Roubos <donovan@loyals.nl>
2018-01-14 21:25:34 +0100

    Add style variables object

---
+++ renderer/styles/variables.js
@@ -0,0 +1,28 @@
+export default {
+	colors: {
+		white: '#FFF',
+		grey: '#F3F3F3',
+		dark: '#424459',
+		font: {
+			light: '#CDCDE0',
+			medium: '#7D7D7D',
+			semiMedium: '#7A848F',
+			dark: '#424459',
+		}
+	},
+	sizes: {
+		borderRadius: {
+			small: '2px',
+			medium: '4px',
+			large: '8px'
+		},
+		font: {
+			extraSmall: '8px',
+			small: '10px',
+			semiSmall: '12px',
+			medium: '14px',
+			large: '22px',
+			extraLarge: '32px'
+		}
+	},
+}
